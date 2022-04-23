3424a32f863895ec3b479ece3293342f83853f7b
Drew Page <drew@siegemedia.com>
2022-04-23 12:16:41 -0700

    Updating color palette to Peach

--- src/client/src/theme.ts
+++ src/client/src/theme.ts
@@ -1,17 +1,21 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
 
 // A custom theme for this app
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#5560b9',
+      // main: '#5560b9',
+      // Coral https://www.canva.com/colors/color-palettes/toasted-peach/
+      main: "#F67B50",
     },
     secondary: {
-      main: '#FFE5B4',
+      // main: '#FFE5B4',
+      // Peach
+      main: "#FBC490",
     },
     error: {
-      main: red.A400,
+      // Scarlet
+      main: "#A82810",
     },
   },
 });
