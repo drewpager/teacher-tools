02c7ba689828bbe29dd6624cdb864cfda97ff938
Drew Page <drew@siegemedia.com>
2022-04-23 12:16:09 -0700

    Adding search component

---
+++ src/client/src/lib/components/Search/index.tsx
@@ -0,0 +1,22 @@
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Box } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+export const Search = () => {
+  const history = useNavigate();
+
+  const onSearch = (value: string, e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    history(`/playlist/${value}`)
+  }
+
+  const [input, setInput] = useState('search playlists');
+
+  return (
+    <Box sx={{ marginLeft: 5 }}>
+      <TextField variant='outlined' sx={{ width: 500 }} placeholder={input} onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setInput(`${e.target.value}`)} />
+      <Button onClick={(event: FormEvent<any>): void => onSearch(input, event) }><SearchIcon /></Button>
+    </Box>
+  )
+}
