c0360bbc4cbe77fe72a9dcbd44a463528e8042ea
Drew Page <drewpage@Drews-iMac.local>
2022-05-11 05:39:57 -0700

    adding graphql queries for allLessons

---
+++ src/client/src/lib/graphql/queries/Lessons/index.ts
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-tag';
+
+export const ALL_LESSONS = gql`
+  query AllLessons($limit: Int!, $page: Int!) {
+    allLessons(limit: $limit, page: $page) {
+      total
+      result {
+        id
+        category
+        title
+        meta
+        video
+        image
+        startDate
+        endDate
+        creator
+      }
+    }
+  }
+`;
