546ec390739ffb9a3f7dc55a1314a37fafa6e88d
Drew Page <drewpage@Drews-iMac.local>
2022-06-01 07:58:17 -0700

    Working on PlaylistCard component

--- src/client/src/lib/components/PlaylistCard/index.tsx
+++ src/client/src/lib/components/PlaylistCard/index.tsx
@@ -1,22 +1,24 @@
 import React, { useState, SyntheticEvent } from 'react';
-import { Box, Accordion, AccordionDetails, AccordionSummary, Typography, Grid, Card, CardMedia } from '@mui/material';
+import { Box, Accordion, AccordionDetails, AccordionSummary, Typography, Grid, Card, CardMedia, Chip } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Lesson } from '../../../graphql/generated';
 import { VideoPlayer } from '../index';
 // import { Playlist } from '../../../graphql/generated';
 
+// NOTE: Pass lessons object instead of single lesson for Accordion to work correctly
 export const PlaylistCard = (lesson: Lesson) => {
   const [expanded, setExpanded] = useState<string | false>(false);
 
-  const handleChange = (panel: string) => (event: SyntheticEvent, newExpanded: boolean) => {
-    setExpanded(newExpanded ? panel : false);
-  }
+  const handleChange = (panel: string) => (event: SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false);
+    console.log(expanded)
+  };
   
   return (
     <Grid container spacing={2}>
       <Grid item xs={4}>
         <Box sx={{ marginBottom: 1, maxWidth: "100%" }}>
-          <Accordion expanded={expanded === `panel${lesson.id}`} onChange={handleChange(`panel${lesson.id}`)}>
+          <Accordion expanded={expanded === `${lesson.title}`} onChange={handleChange(`${lesson.title}`)}>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls={`panel${lesson.id}bh-content`}
@@ -28,7 +30,7 @@ export const PlaylistCard = (lesson: Lesson) => {
             </AccordionSummary>
             <AccordionDetails>
               <Typography sx={{ color: 'black' }}>
-                {lesson.video}
+                {lesson.category?.map((i) => (<Chip label={i} sx={{ marginRight: 1 }}/>))}
               </Typography>
               <Typography sx={{ color: 'text.secondary' }}>{lesson.startDate} to {lesson.endDate}</Typography>
             </AccordionDetails>
