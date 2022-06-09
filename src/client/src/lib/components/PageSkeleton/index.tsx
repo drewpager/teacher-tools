1828389ebb503bd15dca95b97439f3821a2dbca3
Drew Page <drewpage@Drews-iMac.local>
2022-06-09 06:35:37 -0700

    Improved user page skeleton to better match loading state

--- src/client/src/lib/components/PageSkeleton/index.tsx
+++ src/client/src/lib/components/PageSkeleton/index.tsx
@@ -1,18 +1,31 @@
-import { Skeleton, Card, Divider, Box } from '@mui/material';
+import { Skeleton, Card, Divider, Box, Grid } from '@mui/material';
 
 export const PageSkeleton = () => {
   return (
     <>
       <Box sx={{ marginLeft: 5 }}>
       <h1>User Profile</h1>
-      <Card sx={{ minWidth: 275, width: 1/3, boxShadow: 1, padding: 5, margin: 5 }}>
+      <Card sx={{ minWidth: 430, minHeight: 454, width: 1/3, boxShadow: 1, padding: 5, margin: 5 }}>
           <Skeleton variant="circular" width={56} height={56} />
           <Divider />
           <Skeleton variant="text" />
           <Skeleton variant="text" />
           <Skeleton variant="text" />
           <Divider />
-        </Card>
+      </Card>
+      <h2>Lessons</h2>
+      <Grid container>
+        <Grid item spacing={2} xs={1} sm={2} lg={3} xl={3}>
+          <Skeleton variant="rectangular" width={300} height={264}/>
+        </Grid>
+        <Grid item spacing={2} xs={1} sm={2} lg={3} xl={3}>
+          <Skeleton variant="rectangular" width={300} height={264}/>
+        </Grid>
+        <Grid item spacing={2} xs={1} sm={2} lg={3} xl={3}>
+          <Skeleton variant="rectangular" width={300} height={264}/>
+        </Grid>
+      </Grid>
+      
       </Box>
     </>
   )
