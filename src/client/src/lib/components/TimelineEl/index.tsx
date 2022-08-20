e5acc79105c289bfceb39d316aa635ff1100c3ad
drewpager <drewpage@shredlights.com>
2022-08-20 08:37:59 -0700

    Adding MUI/lab for Timeline component

--- src/client/src/lib/components/TimelineEl/index.tsx
+++ src/client/src/lib/components/TimelineEl/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, Typography } from '@mui/material';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineOppositeContent, TimelineDot } from '@mui/lab';
 import { Lesson, useAllLessonsQuery } from '../../../graphql/generated';
 import { DisplayError } from '../../utils';
 
@@ -45,11 +46,34 @@ export const TimelineEl = () => {
   return (
     <Box sx={{ marginTop: 5 }}>
       <Typography variant="h4">Teach History Chronologically</Typography>
-      <ul>
-        {start.map((i, index) => (
-          <li key={index}>{i.title}</li>
-        ))}
-      </ul>
+      {start.map((i) => (
+        <Box>
+          <Timeline position='right'>
+          <TimelineItem>
+            <TimelineOppositeContent
+              sx={{ m: 'auto 0' }}
+              align="right"
+              variant="body2"
+              color="text.secondary"
+            >
+              {i.startDate}
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+                <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+              <Typography variant="h6" component="span">
+                {i.title}
+              </Typography>
+              <Typography>{i.meta}</Typography>
+            </TimelineContent>
+          </TimelineItem>
+    
+          </Timeline>
+        </Box>
+      ))}
     </Box>
   )
 }
