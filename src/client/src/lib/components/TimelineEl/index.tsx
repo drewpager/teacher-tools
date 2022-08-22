99dff86627284dce4f4c6c188354f6c60ad04755
drewpager <drewpage@shredlights.com>
2022-08-22 07:45:43 -0700

    Started timeline filter functionality

--- src/client/src/lib/components/TimelineEl/index.tsx
+++ src/client/src/lib/components/TimelineEl/index.tsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, Button, CircularProgress, Typography } from '@mui/material';
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineOppositeContent, TimelineDot } from '@mui/lab';
 import { Lesson, useAllLessonsQuery } from '../../../graphql/generated';
 import { DisplayError } from '../../utils';
 
 export const TimelineEl = () => {
   const [start, setStart] = useState<Lesson[]>([]);
+  const [category, setCategory] = useState<string>("")
+  const [categoryList, setCategoryList] = useState<string[]>([""])
+
+  const handleClick = (category: string) => {
+    setCategory(category);
+  }
 
   // 1. Get All Start Dates from All Lessons
   const { data, loading, error } = useAllLessonsQuery({ 
@@ -17,18 +23,30 @@ export const TimelineEl = () => {
 
   useEffect(() => {
     const sorted: Lesson[] = [];
+    const categories: string[] = [];
     const res = data?.allLessons.result;
 
     res?.map((i) => (
       sorted.push(i)
     ))
+
+    // 1.5 get categories
+    sorted.map((i) => (
+      categories.push(`${i.category}`)
+    ))
     
-    // 2. O rganize in descending order
+    // 2. Organize in descending order
     sorted.sort((a: any, b: any) => {
       return a.startDate - b.startDate;
     })
+
+    sorted.filter((l) => l.category?.toString().toLowerCase().includes(category.toLowerCase()))
+
+    let uniqueCategories = Array.from(new Set(categories));
     setStart(sorted);
-  }, [data])
+    setCategoryList(uniqueCategories);
+
+  }, [data, category])
 
   if (loading) {
     return (
@@ -46,6 +64,7 @@ export const TimelineEl = () => {
   return (
     <Box sx={{ marginTop: 5 }}>
       <Typography variant="h4">Teach History Chronologically</Typography>
+      {categoryList.map((j) => (<Button onClick={() => handleClick(j)}>{j}</Button>))}
       {start.map((i) => (
         <Box>
           <Timeline position='right'>
