ebd0d61423afb95b6f12db0db6e6ea6457dbf058
drewpager <drewpage@shredlights.com>
2022-11-03 07:59:45 -0700

    AnswerType radio input selector to determine quiz creation layout

--- src/client/src/sections/CreateQuiz/index.tsx
+++ src/client/src/sections/CreateQuiz/index.tsx
@@ -1,10 +1,13 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, SyntheticEvent } from 'react';
 import { useCreateQuizMutation } from '../../graphql/generated';
-import { Box, TextField } from '@mui/material'
+import { Box, TextField, FormControl, FormLabel, FormControlLabel, Radio, RadioGroup } from '@mui/material'
+import { EnumValueNode } from 'graphql';
+import { AnswerFormat } from '../../graphql/generated';
 
 export const CreateQuiz = () => {
   const [questions, setQuestions] = useState();
   const [quiz, setQuiz] = useState();
+  const [answerType, setAnswerType] = useState<string | undefined>("TRUEFALSE");
   const [title, setTitle] = useState<string>("");
 
   // const [createQuiz] = useCreateQuizMutation({
@@ -17,6 +20,11 @@ export const CreateQuiz = () => {
     t.preventDefault();
     let title = t.target.value;
     setTitle(title)
+  }
+
+  const handleAnswerChange = (e: SyntheticEvent) => {
+    let type = e.currentTarget.getAttribute("value");
+    setAnswerType(type?.toLocaleUpperCase());
   }
 
   const updateQuestions = (t: ChangeEvent<HTMLInputElement>) => {
@@ -38,6 +46,23 @@ export const CreateQuiz = () => {
           sx={{ marginTop: 2 }}
           onChange={updateQuestions}
         />
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label">Answer Type</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="TRUEFALSE"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="TRUEFALSE" control={<Radio />} label="True/False" onChange={(e) => handleAnswerChange(e)} />
+            <FormControlLabel value="MULTIPLECHOICE" control={<Radio />} label="Multiple Choice" onChange={(e) => handleAnswerChange(e)} />
+          </RadioGroup>
+        </FormControl>
+        {answerType === "TRUEFALSE" ? (
+          <h2>True False!</h2>
+        ) : <></>}
+        {answerType === "MULTIPLECHOICE" ? (
+          <h2>Multiple Choice!</h2>
+        ) : <></>}
       </form>
     </Box>
   )
