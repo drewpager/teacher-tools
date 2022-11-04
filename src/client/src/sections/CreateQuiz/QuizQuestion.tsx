d056e81ba8a4fa43e337ebb63b0c9d4bb4c493c4
drewpager <drewpage@shredlights.com>
2022-11-04 07:50:48 -0700

    Rendering two answerType layouts, light styling, and started Add question functionality

--- src/client/src/sections/CreateQuiz/QuizQuestion.tsx
+++ src/client/src/sections/CreateQuiz/QuizQuestion.tsx
@@ -1,54 +1,29 @@
-import React, { useState, ChangeEvent, SyntheticEvent } from 'react';
-import { QuizQuestion } from './QuizQuestion';
-import { useCreateQuizMutation } from '../../graphql/generated';
-import { Box, TextField, FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, Typography, Tooltip, Button } from '@mui/material'
+import React, { useState, SyntheticEvent, ChangeEvent } from "react";
+import { TextField, FormControl, FormLabel, FormControlLabel, Radio, RadioGroup, Typography, Tooltip } from '@mui/material'
 import { CheckCircle, Cancel } from '@mui/icons-material';
 import './createQuiz.scss';
 
-export const CreateQuiz = () => {
-  const [questions, setQuestions] = useState();
-  const [quiz, setQuiz] = useState();
+export const QuizQuestion = () => {
   const [answerType, setAnswerType] = useState<string | undefined>("TRUEFALSE");
   const [answerTrue, setAnswerTrue] = useState<boolean>(true);
-  const [addQuestion, setAddQuestion] = useState<number>(0)
-  const [title, setTitle] = useState<string>("");
-  
-  // const [createQuiz] = useCreateQuizMutation({
-  //   variables: {
-  //     input: quiz
-  //   }
-  // })
-
-  const updateTitle = (t: ChangeEvent<HTMLInputElement>) => {
-    t.preventDefault();
-    let title = t.target.value;
-    setTitle(title)
-  }
 
   const handleAnswerChange = (e: SyntheticEvent) => {
     let type = e.currentTarget.getAttribute("value");
     setAnswerType(type?.toLocaleUpperCase());
   }
-
+  
   const updateQuestions = (t: ChangeEvent<HTMLInputElement>) => {
-
+  
   }
-
+  
   const updateAnswers = (t: ChangeEvent<HTMLInputElement>) => {
     console.log(t.target.value)
   }
 
   return (
-    <Box sx={{ marginTop: 10, maxWidth: "50%" }}>
-      <h1>Create Assessment</h1>
-      <form>
-        <TextField 
-          label="Enter Quiz Title"
-          fullWidth
-          onChange={updateTitle}
-        />
-        <TextField 
-          label="Enter First Question"
+    <div>
+    <TextField 
+          label="Enter Question"
           fullWidth
           sx={{ marginTop: 2 }}
           onChange={updateQuestions}
@@ -128,10 +103,6 @@ export const CreateQuiz = () => {
           </div>
         </div>
         ) : <></>}
-        {addQuestion ? <QuizQuestion /> : <></>}
-        {/* {addQuestion ? Array(addQuestion).map((_, i) => (<QuizQuestion />)) : <></>} */}
-        <Button onClick={() => setAddQuestion(addQuestion + 1)}>Add Question</Button>
-      </form>
-    </Box>
+    </div>
   )
 }
