57552e7a0084c59c38a781759532547218ffd713
drewpager <drewpage@shredlights.com>
2022-12-28 13:54:03 -0800

    updating footer with logo

--- src/client/src/lib/components/Footer/index.tsx
+++ src/client/src/lib/components/Footer/index.tsx
@@ -1,7 +1,8 @@
-import { Box, CssBaseline, Container, Typography, Grid, Chip } from '@mui/material';
+import { Box, CssBaseline, Container, Typography, Grid, Chip, SvgIcon } from '@mui/material';
 import { Viewer } from '../../../graphql/generated';
 import { Copyright } from '@mui/icons-material';
-import React from 'react';
+import { ReactComponent as PlatosPeachIcon } from '../../assets/peach-logo.svg';
+import theme from '../../../theme';
 import './footer.scss';
 import { Link } from 'react-router-dom';
 
@@ -59,10 +60,13 @@ export const Footer = ({ viewer }: Props) => {
             </Link>
           </Grid>
         </Grid>
+        <Box className="footer--icon-center">
+        <SvgIcon component={PlatosPeachIcon} inheritViewBox sx={{ fontSize: 150, color: `${theme.palette.primary.light}` }} />
         <Typography className='footer--text-center'>
             Plato's Peach
           <Copyright fontSize='small'/>
         </Typography>
+        </Box>
       </Box>
   )
 }
