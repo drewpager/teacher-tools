2b20aa8c96a1d7e6d6ded53d9e6ba67ecde07167
drewpager <drewpage@shredlights.com>
2023-01-15 12:25:45 -0800

    persisting on tweaks to test

--- src/server/index.ts
+++ src/server/index.ts
@@ -23,8 +23,8 @@ const mount = async (app: Application) => {
   app.use(cors(corsOptions));
   app.use(cookieParser(process.env.SECRET));
   app.use(compression());
-  app.use(express.static(`${__dirname}/static/`));
-  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/index.html`));
+  // app.use(express.static(`${__dirname}/static/`));
+  // app.get("/*", (_req, res) => res.sendFile(`${__dirname}/index.html`));
 
   const server = new ApolloServer({
     typeDefs: [typeDefs, ...scalarTypeDefs],
