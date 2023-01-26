e3155b79707aef67c9bee9eeb778e6c1c6a130da
drewpager <drewpage@shredlights.com>
2023-01-26 08:01:49 -0800

    Started quiz player validation and scoring

--- src/client/src/lib/components/QuizPlayer/index.tsx
+++ src/client/src/lib/components/QuizPlayer/index.tsx
@@ -7,25 +7,48 @@ interface Props {
 }
 
 export const QuizPlayer = ({ quiz }: Props) => {
-  
+  const [quizState, setQuizState] = useState<boolean[]>([]);
+
   const title = quiz.title;
+
+  const handleCheck = () => {
+
+  }
+
+  const handleSelect = (z: any) => {
+    setQuizState([...quizState, z.isCorrect])
+  }
+
+  // TODO: 
+  // 1. Randomize order of answerOptions
+  // 2. Check answers + Show Errors/Correct
+  // Considerations:
+  // 1. Allow teachers to set # of attempts before locking in grade?
 
   return (
     <FormControl>
       <form>
-      <h1>{title}</h1>
-      {quiz.questions.map((i, indy) => (
-        <>
-          <RadioGroup key={indy}>
-            <h2>{i.question}</h2>
-            {i.answerOptions?.map((t, index) => (
-              <FormControlLabel value={t?.answerText} label={`${t?.answerText}`} key={index} control={<Radio />} />
-            ))}
-          </RadioGroup>
-        </>
-      ))}
-      <Button type="submit">Check Answers</Button>
-    </form>
+        <h1>{title}</h1>
+        {console.log(quiz)}
+        {quiz.questions.map((i, indy) => (
+          <>
+            <RadioGroup key={indy}>
+              <h2>{i.question}</h2>
+              {i.answerType === "MULTIPLECHOICE" && i.answerOptions?.map((t, index) => (
+                <FormControlLabel value={t?.answerText} label={`${t?.answerText}`} key={index} control={<Radio onSelect={(t) => handleSelect(t)} />} />
+              ))}
+              {i.answerType === "TRUEFALSE" && i.answerOptions?.map((f, index) => (
+                <>
+                  <FormControlLabel value={f?.isCorrect?.valueOf()} label={`${f?.isCorrect?.valueOf()}`} key={index} control={<Radio onSelect={(f) => handleSelect(f)} />} />
+                  <FormControlLabel value={!f?.isCorrect?.valueOf()} label={`${!f?.isCorrect?.valueOf()}`} key={index} control={<Radio onSelect={(f) => handleSelect(f)} />} />
+                </>
+              ))}
+              {console.log(quizState)}
+            </RadioGroup>
+          </>
+        ))}
+        <Button type="button" onClick={handleCheck}>Check Answers</Button>
+      </form>
     </FormControl>
   )
 }
