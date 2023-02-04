db0effcb8c1223a027a9d8b922aea1afd9798be7
drewpager <drewpage@shredlights.com>
2023-02-04 10:53:09 -0800

    Added util function for formatting lowercase text to Title Case

---
+++ src/client/src/lib/utils/titleCase.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const titleCase = (str: string) => {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
