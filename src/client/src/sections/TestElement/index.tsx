70a0e241b0612b7a153a708f079bd35329be36fc
drewpager <drewpage@shredlights.com>
2023-03-28 07:23:02 -0700

    Playing around with CSS tricks

--- src/client/src/sections/TestElement/index.tsx
+++ src/client/src/sections/TestElement/index.tsx
@@ -1,152 +1,107 @@
 // import React from 'react';
-// import { AnswerFormat } from '../../graphql/generated';
-// import { QuizPlayer } from '../../lib/components';
+// // import './pricing.css';
 
+// // Define the pricing plan options
+// const pricingPlans = [{
+//   name: 'Basic', price: '$9.99/month', features: ['5 GB of storage', '10 email accounts', '24/7 support',],
+// },
+// {
+//   name: 'Standard',
+//   price: '$19.99/month',
+//   features: [
+//     '10 GB of storage',
+//     '20 email accounts',
+//     '24/7 support',
+//     'Priority customer support',
+//   ],
+// },
+// {
+//   name: 'Premium',
+//   price: '$49.99/month',
+//   features: [
+//     '50 GB of storage',
+//     'Unlimited email accounts',
+//     '24/7 support',
+//     'Priority customer support',
+//     'Dedicated account manager',
+//   ],
+// },
+// ];
+
+// // Define the FAQ items
+// // const faqItems = [  {    question: 'What is your refund policy?',    answer: 'We offer a 30-day money-back guarantee for all our plans.',  },  {    question: 'Can I switch plans at any time?',    answer: 'Yes, you can switch plans at any time. Your new plan will take effect immediately.',  },  {    question: 'Do you offer custom plans for large organizations?',    answer: 'Yes, we offer custom plans for organizations with specific needs. Please contact us for more information.',  },];
+
+// // Pricing component
 // export const TestElement = () => {
-//   const name = "Drew"
-//   const quiz = {
-//     creator: "112129642735396482304",
-//     id: "0101",
-//     title: "Test Quiz For Drew!",
-//     questions: [
-//       {
-//         question: "What is this about",
-//         answerType: AnswerFormat.Truefalse,
-//         answerOptions: [
-//           { answerText: "True", isCorrect: false },
-//           { answerText: "False", isCorrect: true },
-//         ]
-//       },
-//       {
-//         question: "Now what about this",
-//         answerType: AnswerFormat.Multiplechoice,
-//         answerOptions: [
-//           { answerText: "Drew", isCorrect: true },
-//           { answerText: "Steve", isCorrect: false },
-//           { answerText: "Katie", isCorrect: true },
-//           { answerText: "Karen", isCorrect: false },
-//         ]
-//       }
-//     ],
-//   }
-
-//   // Render your list
 //   return (
-//     <div style={{ marginTop: 100 }}>
-//       <h2>{name}'s Test Page</h2>
-//       <QuizPlayer quiz={quiz} />
+//     <div className="pricing">
+//       <h1>Choose Your Plan</h1>
+//       <div className="pricing-plans">
+//         {pricingPlans.map(plan => (
+//           <div className="pricing-plan" key={plan.name}>
+//             <h2>{plan.name}</h2>
+//             <p className="price">{plan.price}</p>
+//             <ul className="features">
+//               {plan.features.map(feature => (
+//                 <li key={feature}>{feature}</li>
+//               ))}
+//             </ul>
+//             <button className="btn">Sign Up</button>
+//           </div>
+//         ))}
+//       </div>
+//       <h2>Product Comparison</h2>
+//       <table className="comparison">
+//         <thead>
+//           <tr>
+//             <th></th>
+//             <th>Basic</th>
+//             <th>Standard</th>
+//             <th>Premium</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>Price</td>
+//             <td>$9.99/month</td>
+//             <td>$19.99/month</td>
+//             <td>$49.99/month</td>
+//           </tr>
+//           <tr>
+//             <td>Storage</td>
+//             <td>5 GB</td>
+//             <td>10 GB</td>
+//             <td>50 GB</td>
+//           </tr>
+//           <tr>
+//             <td>Email Accounts</td>
+//             <td>10</td>
+//             <td>20</td>
+//             <td>Unlimited</td>
+//           </tr>
+//           <tr>
+//             <td>Support</td>
+//             <td>24/7</td>
+//             <td>24/7</td>
+//             <td>24/7</td>
+//           </tr>
+//         </tbody>
+//       </table>
 //     </div>
-//   );
+//   )
 // }
 
-import React, { useState } from "react";
-import { AnswerFormat } from '../../graphql/generated';
-import { Button } from '@mui/material'
+import React from "react";
+import image from '../../lib/assets/classroom.jpg';
 
-const quiz = {
-  creator: "112129642735396482304",
-  id: "0101",
-  title: "Test Quiz For Drew!",
-  questions: [
-    {
-      id: 1,
-      question: "What is Chamath's Last Name?",
-      answerType: AnswerFormat.Multiplechoice,
-      answerOptions: [
-        { answerText: "Palihapitiya", isCorrect: true },
-        { answerText: "Steve", isCorrect: false },
-        { answerText: "Branson", isCorrect: false },
-        { answerText: "Karen", isCorrect: false },
-      ]
-    },
-    {
-      id: 2,
-      question: "What is this about",
-      answerType: AnswerFormat.Truefalse,
-      answerOptions: [
-        { answerText: "True", isCorrect: false },
-        { answerText: "False", isCorrect: true },
-      ]
-    },
-    {
-      id: 3,
-      question: "Now what about this",
-      answerType: AnswerFormat.Multiplechoice,
-      answerOptions: [
-        { answerText: "Drew", isCorrect: true },
-        { answerText: "Steve", isCorrect: false },
-        { answerText: "Katie", isCorrect: true },
-        { answerText: "Karen", isCorrect: false },
-      ]
-    }
-  ],
-}
-
-// Define a type for the quiz state
-type QuizState = {
-  currentQuestionIndex: number;
-  userAnswers: { [key: number]: string };
-};
-
-// Define the Quiz component
-export const TestElement: React.FC = () => {
-  // Initialize the quiz state
-  const [quizState, setQuizState] = useState<QuizState>({
-    currentQuestionIndex: 0,
-    userAnswers: {},
-  });
-
-  // Get the current question from the questions array
-  const currentQuestion = quiz.questions[quizState.currentQuestionIndex];
-
-  // Handle the user's answer to the current question
-  const handleAnswer = (answer: string) => {
-    setQuizState((prevState) => ({
-      ...prevState,
-      userAnswers: {
-        ...prevState.userAnswers,
-        [currentQuestion.id]: answer,
-      },
-    }));
-  };
-
-  // Handle the user clicking the "Next" button
-  const handleNextQuestion = () => {
-    setQuizState((prevState) => ({
-      ...prevState,
-      currentQuestionIndex: prevState.currentQuestionIndex + 1,
-    }));
-  };
-
-  // Calculate the user's score
-  const score = Object.keys(quizState.userAnswers).reduce((acc, key) => {
-    if (quizState.userAnswers[Number(key)] === quiz.questions[Number(key)].answerOptions.find((q) => q.isCorrect)?.answerText) {
-      return acc + 1;
-    } else {
-      return acc;
-    }
-  }, 0);
-
-  // Render the current question
+export const TestElement = () => {
   return (
-    <div style={{ marginTop: 50 }}>
-      <h1>{quiz.title}</h1>
-      <h2>{currentQuestion.question}</h2>
-      {currentQuestion.answerOptions.map((choice, inder) => (
-        <div key={inder}>
-          <input
-            type={currentQuestion.answerType === AnswerFormat.Truefalse ? "radio" : "checkbox"}
-            name="answer"
-            value={choice.answerText}
-            onChange={(e) => handleAnswer(e.target.value)}
-          />
-          <label>{choice.answerText}</label>
-        </div>
-      ))}
-      <div>
-        <p>Your score: {score} out of {quiz.questions.length}</p>
-      </div>
-      {quizState.currentQuestionIndex + 1 < quiz.questions.length && (<Button variant="outlined" onClick={handleNextQuestion}>Next</Button>)}
+    <div style={{
+      marginTop: 150, width: "550px", height: "auto", border: "5px solid red",
+      boxShadow: "2px 2px 10px rgba(0,0,0,0.5)",
+      // filter: "drop-shadow(2px 2px 10px rgba(0,0,0,0.5)"
+    }}>
+      <img src={image} alt="classroom" style={{ transition: ".5s filter", filter: `sepia() saturate(4) hue-rotate(195deg)` }} />
     </div>
-  );
-};
+  )
+}
