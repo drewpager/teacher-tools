3a654ad980634adf3a56114b2edc95d26421d74d
drewpager <drewpage@shredlights.com>
2023-05-07 07:31:30 -0700

    Adding reverse and chronological sorting functions and switch for UX

--- src/client/src/sections/Catalog/index.tsx
+++ src/client/src/sections/Catalog/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, CircularProgress, Grid, Typography, Chip } from '@mui/material'
+import { Box, CircularProgress, Grid, Typography, Chip, Switch } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import AdjustIcon from '@mui/icons-material/Adjust';
@@ -18,6 +18,7 @@ type Props = {
 export const Catalog = ({ viewer }: Props) => {
   const [expanded, setExpanded] = React.useState<string[]>([]);
   const [selected, setSelected] = React.useState<string[]>(['ancient history']);
+  const [ascending, setAscending] = React.useState<boolean>(true);
 
   const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
     setExpanded(nodeIds);
@@ -64,23 +65,31 @@ export const Catalog = ({ viewer }: Props) => {
     return value.secondary !== undefined
   }
 
+  function ascend(a: any, b: any) {
+    return new Date(a.startDate).getTime() - new Date(b.startDate).getTime();
+  }
+
+  function descend(a: any, b: any) {
+    return new Date(b.startDate).getTime() - new Date(a.startDate).getTime();
+  }
+
   // Isolate the main and any secondary categories
   const categor = data?.allLessons.result;
   const mainCategoryArray: any[] = [];
   const secondaryCategory: any = [{}];
   const allCategories: any[] = [];
-  // categor?.sort((a, b) => a.startDate - b.startDate)
+
   categor?.map((i) => mainCategoryArray.push(i?.category ? i.category[0]?.trim() : undefined))
   categor?.map((i) => allCategories.push(i?.category ? i.category.map(item => item?.trim()) : undefined))
   categor?.map((i) => secondaryCategory.push(i?.category ? { main: i.category[0], secondary: i.category[1]?.trim() } : undefined))
   categor?.map((i) => secondaryCategory.push(i?.category ? { main: i.category[0], secondary: i.category[2]?.trim() } : undefined))
   categor?.map((i) => secondaryCategory.push(i?.category ? { main: i.category[0], secondary: i.category[3]?.trim() } : undefined))
-  let mainCategories = mainCategoryArray.filter(onlyUnique).sort()
+  const mainCategories = mainCategoryArray.filter(onlyUnique).sort()
   let secCategories = secondaryCategory.filter(onlyDefined)
   const secondaryCategories = new Map(secCategories.map((item: any) =>
     [item["secondary"], item])).values();
 
-  let combinedCategories = Array.from(secondaryCategories).sort((a: any, b: any) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime())
+  const combinedCategories = Array.from(secondaryCategories)
   const selectedSecondary = allCategories.filter((b) => b.includes(selected[0]));
   return (
     <Box>
@@ -122,14 +131,17 @@ export const Catalog = ({ viewer }: Props) => {
         <Grid item sm={12} md={9} lg={9}>
           <Box className="catalogBackground" sx={{ marginBottom: "80px" }} id={`${selected[0]}`}>
             <h1 className="catalogTitle">Catalog <Chip label={data?.allLessons.total} color="primary" size="medium" /></h1>
+            <Box sx={{ display: 'flex' }}>
+              <Switch checked={ascending} onChange={() => setAscending(!ascending)} /><Typography variant='h3'>{ascending ? "Chronological" : "Reverse Chronological"}</Typography>
+            </Box>
             {selected && data && (
               <div className="catalog--item">
-                <CatalogItem viewer={`${viewer.id}`} name={`${selected[0]}`} category={data.allLessons.result.filter((b) => b.category?.includes(selectedSecondary[0][1] ? ` ${selectedSecondary[0][1]}` : selected[0])).sort((a: any, b: any) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime())} key={`${selected[0]}`} />
+                <CatalogItem viewer={`${viewer.id}`} name={`${selected[0]}`} category={data.allLessons.result.filter((b) => b.category?.includes(selectedSecondary[0][1] ? ` ${selectedSecondary[0][1]}` : selected[0])).sort(ascending ? ascend : descend)} key={`${selected[0]}`} />
               </div>
             )}
             {data && categories.map((cater) => (
               <div className="catalog--item">
-                <CatalogItem viewer={`${viewer.id}`} name={cater.name} category={data.allLessons.result.filter((b) => b.category?.includes(cater.name)).sort((a: any, b: any) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime())} key={cater.name} />
+                <CatalogItem viewer={`${viewer.id}`} name={cater.name} category={data.allLessons.result.filter((b) => b.category?.includes(cater.name)).sort(ascending ? ascend : descend)} key={cater.name} />
               </div>
             ))}
           </Box >
