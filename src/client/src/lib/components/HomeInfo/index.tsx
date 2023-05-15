623ccadc61c93b9207f08db70d91da1155dbee55
drewpager <drewpage@shredlights.com>
2023-05-15 08:06:35 -0700

    Redesign: Desktop home page styling

--- src/client/src/lib/components/HomeInfo/index.tsx
+++ src/client/src/lib/components/HomeInfo/index.tsx
@@ -37,7 +37,12 @@ export const HomeInfo = () => {
         </Box>
       </Grid>
       <Grid item xs={0} sm={0} md={5} lg={6}>
-        <div className="contain" ref={contain} />
+        <Box className="animation--box">
+          <svg width="459" height="450" viewBox="0 0 109 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M0.824206 52.6526C0.390436 32.7863 11.1055 14.338 29.6406 7.01791C52.847 -2.14699 81.5221 -4.87401 96.9357 14.6952C113.727 36.0129 111.01 67.3815 93.551 88.159C78.8336 105.674 52.9055 100.751 31.5083 92.558C14.1551 85.9132 1.22866 71.1762 0.824206 52.6526Z" fill="#FFFFFF" fill-opacity="0.9" />
+          </svg>
+          <div className="contain" ref={contain} />
+        </Box>
       </Grid>
     </Grid>
   );
