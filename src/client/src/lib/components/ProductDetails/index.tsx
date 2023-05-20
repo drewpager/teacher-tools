5ae8aef3173ee8f30f252ba12d986b2cca564820
drewpager <drewpage@shredlights.com>
2023-05-20 11:11:40 -0700

    Redesign: Styling home page for desktop

--- src/client/src/lib/components/ProductDetails/index.tsx
+++ src/client/src/lib/components/ProductDetails/index.tsx
@@ -12,14 +12,14 @@ import './productDetails.scss';
 export const ProductDetails = () => {
   return (
     <Box className="details--box">
-      <Typography variant="h2" className="productDetails-title--font">How Plato's Peach Works</Typography>
+      <Typography variant="h2" className="productDetails-heading--font">How Plato's Peach Works</Typography>
       <Grid container>
         <Grid item xs={12} sm={12} md={8}>
           <Box className="details-blue--box">
             <Box>
               <img src={sistine} alt="Plato's Peach Video Player of Sistine Chapel" className="img--overlay" />
             </Box>
-            <Box sx={{ position: "relative" }}>
+            <Box className="productDetails-number--box">
               <svg width="69" height="60" viewBox="0 0 109 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M0.824206 52.6526C0.390436 32.7863 11.1055 14.338 29.6406 7.01791C52.847 -2.14699 81.5221 -4.87401 96.9357 14.6952C113.727 36.0129 111.01 67.3815 93.551 88.159C78.8336 105.674 52.9055 100.751 31.5083 92.558C14.1551 85.9132 1.22866 71.1762 0.824206 52.6526Z" fill="#FFFFFF" fill-opacity="0.9" />
               </svg>
@@ -28,7 +28,7 @@ export const ProductDetails = () => {
             <Typography variant="h2" className="productDetails-subtitle--font">
               Browse Our Growing Library of History Documentaries
             </Typography>
-            <Typography variant="body2" sx={{ pb: "2rem", pr: "2rem", fontSize: 20 }}>
+            <Typography variant="body2" className="productDetails-description--font">
               Using our <Link to="/playlist/create">lesson plan creation tool</Link> you can quickly construct lesson plans with the content you choose and assessments you create.
             </Typography>
             <Button href='/catalog' className="pricing--button">View Catalog</Button>
