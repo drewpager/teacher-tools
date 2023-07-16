5af49302d3dcc47de2f74f8d20cb0bccfbe7193c
drewpager <drewpage@shredlights.com>
2023-07-16 08:34:04 -0700

    Resloving console error regarding rows vs maxRows conflict

--- src/client/src/sections/Contact/ContactForm.tsx
+++ src/client/src/sections/Contact/ContactForm.tsx
@@ -45,7 +45,7 @@ export const ContactForm = () => {
           <label htmlFor="email">Email</label>
           <TextField type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           <label htmlFor="message">Message</label>
-          <TextField multiline rows={3} maxRows={25} id="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Send product feedback, feature requests, or any other correspondence for the Plato's Peach team!" required />
+          <TextField multiline maxRows={25} id="message" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Send product feedback, feature requests, or any other correspondence for the Plato's Peach team!" required />
           <Button variant="contained" className="contact--button" type="submit">Submit</Button>
           {status && <p>Message {status}!</p>}
         </Box>
