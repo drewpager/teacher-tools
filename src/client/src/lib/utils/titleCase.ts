2819c0400942a4d928cca5033bb3a08bf2d90ce4
drewpager <drewpage@shredlights.com>
2023-07-16 07:39:43 -0700

    Handling title case edge cases

--- src/client/src/lib/utils/titleCase.ts
+++ src/client/src/lib/utils/titleCase.ts
@@ -4,6 +4,27 @@ export const titleCase = (str: string) => {
   if (str === "u.s. constitution") {
     return "U.S. Constitution";
   }
+  if (str === "mexican-american war") {
+    return "Mexican-American War";
+  }
+  if (str === "spanish-american war") {
+    return "Spanish-American War";
+  }
+  if (str === "sino-soviet war") {
+    return "Sino-Soviet War";
+  }
+  if (str === "post-cold war") {
+    return "Post-Cold War";
+  }
+  if (str === "war of 1812") {
+    return "War of 1812";
+  }
+  if (str === "age of exploration") {
+    return "Age of Exploration";
+  }
+  if (str === "tradition of taps") {
+    return "Tradition of Taps";
+  }
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
   });
