12de4e936dd6adc3a04729486fd6b170aff7b77f
drewpager <drewpage@shredlights.com>
2023-10-23 07:26:30 -0700

    Updating PDF JS to legacy version

--- src/client/src/lib/components/PdfPlayer/index.tsx
+++ src/client/src/lib/components/PdfPlayer/index.tsx
@@ -8,7 +8,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
+  'pdfjs-dist/legacy/build/pdf.worker.min.js',
   import.meta.url,
 ).toString();
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
