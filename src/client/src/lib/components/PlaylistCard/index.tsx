9a984965d142a1955f3361642f253b9ef95a2715
drewpager <drewpage@shredlights.com>
2023-12-18 08:03:47 -0800

    Math round for even estimates

--- src/client/src/lib/components/PlaylistCard/index.tsx
+++ src/client/src/lib/components/PlaylistCard/index.tsx
@@ -247,7 +247,7 @@ export const PlaylistCard = ({ playlist, viewer }: Props) => {
         <Tooltip title="Estimtated Completion Time">
           <HistoryToggleOffIcon />
         </Tooltip>
-        <Typography className='playlist--duration' variant="body1">{estimatedTime}-{estimatedTime * 1.5} Minutes</Typography>
+        <Typography className='playlist--duration' variant="body1">{estimatedTime}-{Math.round(estimatedTime * 1.5)} Minutes</Typography>
       </Box>
       {(!viewer?.paymentId || viewer.paymentId === null) && playlist.premium && (<Box className="premium-content hide-premium" />)}
       <Grid container className='playlistcard--grid'>
